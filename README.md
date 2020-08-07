78ef345f439f364669e3ae8423d4748389f90722
GregP <glp@swimr.com>
2020-08-06 18:14:22 -0700

    added lazy option

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # rust-ping-pong
 A UDP ping pong app to practice Rust standard library modules.
+
+On client #1:
+cargo run -- ping
+
+On client #2:
+cargo run -- pong
+
+Or run both in the same process:
+cargo run -- lazy
--- src/main.rs
+++ src/main.rs
@@ -4,48 +4,22 @@
 	A ping client and a pong client each open two sockets and receive on one and send on the other.
 */
 
-use std::process::exit;
-
-fn parse_args() -> Option<String>{
-	let args: Vec<String> = std::env::args().collect();
-	if args.len() <= 1 {
-		print_usage_and_exit();
-		None
-	}else{
-		// print_args(&args);
-		match args[1].as_str() {
-			"ping" => Some(String::from("ping")),
-			"pong" => Some(String::from("pong")),
-			_ => {
-				print_usage_and_exit();
-				None
-			}
-		}
-	}
-}
-
-fn print_usage_and_exit(){
-	println!("Usage: [ping/pong]");
-	exit(0)
-}
-
+/// Send "pings" until the timer runs out; receive "pongs" back.
+/// TODO: Doesn't need two sockets.
 fn run_ping(){
-	println!("[run_ping]");
-
-	std::thread::spawn(move ||{
+	let thread0 = std::thread::spawn(move ||{
 		let socket_out = std::net::UdpSocket::bind("127.0.0.1:11110").expect("couldn't bind to address");
-		// socket_send.connect("0.0.0.0:11111").expect("connect function failed");
 		let mut i:u8 = 0;
 		loop{
 			let mut mesg_vec = vec![];
-			//let ping_message:String= format!("ping{}", i);
 			let ping_bytes:&[u8] = "ping".as_bytes();
 			mesg_vec.extend_from_slice(&ping_bytes);
 			mesg_vec.extend_from_slice(&[i]);
-			println!("sending ping: {:?}", &mesg_vec);
+			println!("[ping] sent ping: {:?}", &mesg_vec);
 			let _send_result = &socket_out.send_to(&mesg_vec, "127.0.0.1:22221");
 			std::thread::sleep(std::time::Duration::from_millis(500));
 
+			// reset the counter
 			match i {
 				0..=254 => i+=1,
 				_ => i = 0,
@@ -53,40 +27,35 @@ fn run_ping(){
 		};
 	});
 
-	// 
-	std::thread::spawn(move ||{
-
+	let thread1 = std::thread::spawn(move ||{
 		let socket_in = std::net::UdpSocket::bind("127.0.0.1:11111").expect("couldn't bind to address");
-		// socket_send.connect("0.0.0.0:11111").expect("connect function failed");
 		let mut buf = [0; 10];
 		loop{
 			match socket_in.recv(&mut buf) {
 				Ok(received) => {
-					println!("received {} bytes {:?}", received, &buf[..received]);
+					// TODO parse the buffer to see if it's a "pong:[counter]"
+					println!("[ping] got pong: {:?}", &buf[..received])
 				},
-				Err(e) => println!("recv function failed: {:?}", e),
+				Err(e) => println!("[ping] recv function failed: {:?}", e),
 			}
-			std::thread::sleep(std::time::Duration::from_millis(1000));
+			// std::thread::sleep(std::time::Duration::from_millis(1000));
 		};
 	});
 
-	// let the other threads run for a while
-	std::thread::sleep(std::time::Duration::from_secs(10));
+	// Don't totally understand rust threads here. One of these is needed to run the pinger. Has no effect in lazy mode.
+	let res = thread0.join();
+	// std::thread::sleep(std::time::Duration::from_millis(1000));
+	// thread1.join().unwrap();
 
-	println!("[run_ping] Started pinger.");
 }
 
-// TODO change the counter size to u64
-// UNDER CONSTRUCTION
+/// Check if the buffer contains a "ping"
 fn parse_ping(buf: &[u8])-> (bool, u8){
-
 	let ping_candidate:&[u8] = &buf[0..=3];
 	let ping_string = String::from_utf8(ping_candidate.to_vec()).unwrap();
 	let ping_count:&u8 = &buf[4];
 	assert_eq!(ping_string, String::from("ping"));
-
 	(true, ping_count.clone())
-
 }
 
 /// Return a one-for-one pong for each ping received
@@ -100,34 +69,52 @@ fn run_pong(){
 	loop{
 		match socket_in.recv(&mut buf) {
 			Ok(byte_count_rcvd) => {
-
-				println!("received {} bytes {:?}", byte_count_rcvd, &buf[..byte_count_rcvd]);
-
-
+				// println!("[pong] received {} bytes {:?}", byte_count_rcvd, &buf[..byte_count_rcvd]);
 				match parse_ping(&buf){
 					(true, ping_id) => {
-						println!("Is it ping? Yes! {}", &ping_id);
+						println!("[pong] got ping: {}, {:?}", &ping_id, &buf[..byte_count_rcvd]);
 						let mut pong_vec = vec![];
 						pong_vec.extend_from_slice("pong".as_bytes());
 						pong_vec.extend_from_slice(&[ping_id]);
-						// let pong_send_result = socket_out.send_to("pong".as_bytes(), "127.0.0.1:11111");
-						let pong_send_result = socket_out.send_to(&pong_vec, "127.0.0.1:11111");
-						println!("Sent pong {:?}", pong_send_result);
+						socket_out.send_to(&pong_vec, "127.0.0.1:11111").unwrap();
+						println!("[pong] sent pong {:?}", &pong_vec);
 					},
-					(false, _) => println!("Is it ping? No."),
-					// _ => println!("Should never get here."),
+					(false, _) => println!("[pong] Datagram was not a ping."),
 				}
 			}
-			Err(e) => println!("recv function failed: {:?}", e),
+			Err(e) => println!("[pong] recv function failed: {:?}", e),
 		}
 	}
 }
 
-fn main() {
-
-	match parse_args().unwrap().as_str() {
-		"ping" => run_ping(),
-		"pong" => run_pong(),
-		_ => (),
+fn parse_args(){
+	let args: Vec<String> = std::env::args().collect();
+	if args.len() <= 1 {
+		print_usage_and_exit();
+	}else{
+		// print_args(&args);
+		match args[1].as_str() {
+			"ping" => run_ping(), // Some(String::from("ping")),
+			"pong" => run_pong(), //Some(String::from("pong")),
+			"lazy" => run_lazy(),
+			_ => print_usage_and_exit(),
+		}
 	}
+}
+
+fn print_usage_and_exit(){
+	println!("Usage: [ping/pong]");
+	std::process::exit(0)
+}
+
+/// Run the ping and pong sides in different threads.
+fn run_lazy(){
+	let pong_child = std::thread::spawn(move ||{ run_pong() });
+	let ping_child = std::thread::spawn(move || { run_ping() });
+	pong_child.join().unwrap();
+	ping_child.join().unwrap();
+}
+
+fn main() {
+	parse_args()
 }
